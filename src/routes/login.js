f05bd5b1671947135e7adc390975434dc9d9fc29
paridhi <paridhi815@gmail.com>
2018-03-05 19:33:35 +0530

    Test Cases written for UserLogin

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,16 +5,78 @@ const url1 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allQues
 const url2 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findAnswerById/';
 
 
-const userLogin = userName => Models.users.findOrCreate({
-  where: {
-    userName,
-  },
-}).then((userExists) => {
-  if (userExists) {
-    return 'New User Is Created';
-  }
-  return 'User Already Exists';
-});
+const userLogin = (userName) => {
+  console.log(userName, 'USERLOGIN');
+
+  const modelPromise = Models.users.findOrCreate({
+    where: {
+      userName,
+    },
+  }).then((userExists) => {
+    console.log(userExists, 'EXISTS');
+    const [user, isCreated] = userExists;
+    if (isCreated) {
+      return 'New User Is Created';
+    }
+    return 'User Already Exists';
+  });
+  return modelPromise;
+};
+
+// const populateDb = () => {
+//     return rp(url1).then((result1) => {
+//         const obj = {};
+//         const questions = JSON.parse(result1);
+//         const promiseArray = [];
+//         for (let i = 0; i < questions.allQuestions.length; i += 1) {
+//           const url = `${url2}${questions.allQuestions[i].questionId}`;
+//           const correctAnswerPromise = rp(url);
+//           promiseArray.push(correctAnswerPromise);
+//           obj.qid = questions.allQuestions[i].questionId;
+//         }
+//         Promise.all(promiseArray).then((result2) => {
+//           for (let i = 0; i < result2.length; i += 1) {
+//             const answer = JSON.parse(result2[i]);
+//             obj.correctAnswer = answer.answer;
+//           }
+//           const optionObj = {};
+//           questions.allQuestions.forEach((question) => {
+//             const questionKeys = Object.keys(question);
+//             questionKeys.forEach((key) => {
+//               if (key.startsWith('option')) {
+//                 optionObj.key = key;
+//                 optionObj[key] = question.key;
+//               }
+//             });
+//           });
+//           const allQuestionsWithOptions = questions.allQuestions.map(question => ({
+//             qid: question.questionId,
+//             options: optionObj,
+//           }));
+//           console.log(allQuestionsWithOptions);
+//         });
+//       });
+// };
+
+// const readDb = () => {
+
+
+// const questionObj={
+//     questionText:
+//     qid:
+//     options:
+// }
+// };
+
+// const ifQuestionsInDb = () => {
+//   Models.questions.findAll().then((result) => {
+//     if (result.length === 0) {
+//       populateDb();
+//     } else {
+//       readDb();
+//     }
+//   });
+// };
 
 const handler = (request, response) => {
   // check if user exists,if existes return user
@@ -22,10 +84,14 @@ const handler = (request, response) => {
   userLogin(request.payload.userName).then((value) => {
     response({
       statusCode: 201,
+      value,
     });
   });
   // check if questions in db,if there then return
   // else populate db
+//   ifQuestionsInDb().then((value) => {
+
+//   });
   // check if ans there in db
   // else populate ans
   // get user answers
--- src/tests/routes/login.test.js
+++ src/tests/routes/login.test.js
@@ -1,13 +1,60 @@
 const Server = require('../../server');
+const Models = require('../../../models');
+
 
 describe('Testing the hapi server for GET request', () => {
-  test('Should return 200 status code for sucessful GET request', (done) => {
+  beforeAll((done) => {
+    Models.users.create({
+      userName: 'Paridhi',
+    }).then(() => {
+      done();
+    });
+  });
+  test('Should return 201 status code for sucessful GET request', (done) => {
     const options = {
       method: 'POST',
       url: '/login',
+      payload: {
+        userName: 'Paridhi',
+      },
     };
     Server.inject(options, (response) => {
       expect(response.result.statusCode).toBe(201);
+      done();
+    });
+  });
+  test('Should return User Already Exists for existing User', (done) => {
+    console.log('REACHED');
+    const options = {
+      method: 'POST',
+      url: '/login',
+      payload: {
+        userName: 'Paridhi',
+      },
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.value).toBe('User Already Exists');
+      done();
+    });
+  });
+  //   test('Should return New User Is Created for New users', (done) => {
+  //     const options = {
+  //       method: 'POST',
+  //       url: '/login',
+  //       payload: {
+  //         userName: 'Pari',
+  //       },
+  //     };
+  //     Server.inject(options, (response) => {
+  //       expect(response.result.value).toBe('New User Is Created');
+  //       done();
+  //     });
+  //   });
+  afterAll((done) => {
+    Models.users.destroy({
+      where: { userName: 'Paridhi' },
+    }).then(() => {
+      console.log('AFTER');
       done();
     });
   });
