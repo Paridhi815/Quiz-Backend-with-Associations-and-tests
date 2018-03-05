bdab2dff14befd2bc6ecd4b2f2d804d7408bfae9
paridhi <paridhi815@gmail.com>
2018-03-05 18:11:08 +0530

    UserLogin function Created

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,27 +5,30 @@ const url1 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allQues
 const url2 = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findAnswerById/';
 
 
-// const userLogin = (userName) => {
-//   Models.users.findOrCreate({
-//     userName,
-//   }).then((userExists) => {
-//     if (userExists) {
-//       status = '';
-//     }
-//   });
-// };
+const userLogin = userName => Models.users.findOrCreate({
+  where: {
+    userName,
+  },
+}).then((userExists) => {
+  if (userExists) {
+    return 'New User Is Created';
+  }
+  return 'User Already Exists';
+});
+
 const handler = (request, response) => {
   // check if user exists,if existes return user
   // else create user and then return
+  userLogin(request.payload.userName).then((value) => {
+    response({
+      statusCode: 201,
+    });
+  });
   // check if questions in db,if there then return
   // else populate db
   // check if ans there in db
   // else populate ans
   // get user answers
-
-  response({
-    statusCode: 201,
-  });
 };
 
 const login = {
